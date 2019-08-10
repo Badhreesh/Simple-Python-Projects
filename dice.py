4a2d1d74f17b5d009b33b72c1cd8801636a43c98
Badhreesh <badhreesh2493@gmail.com>
2019-08-10 18:46:10 +0200

    dice_roll function added

--- dice.py
+++ dice.py
@@ -1,22 +1,21 @@
 from random import randint
 
-def main():
-	min = 1
-
-	max = int(input('How many sided Die do you want to use?'))
-
-	roll = True
-
-	while True:  
-		print('Do you want to roll?')
-		answer = input()
-		if 'y' in answer:
-			print('You rolled a', randint(min, max))
-		elif answer == 'n' or answer == 'no':
+def dice_roll(sides):
+	while True:
+		answer = input('Do you want to roll? (y/n)')
+		if answer == 'y':
+			print('You rolled a', randint(1, sides))
+		elif answer == 'n':
 			print('Thanks for playing')
 			break
 		else:
-			print('Invalid Format. Try Again') 	
+			print('Invalid Format. Try Again')
+
+
+def main():
+	sides = int(input('How many sided Die do you want to use?'))
+	dice_roll(sides)
+	
 
 if __name__ == '__main__':
 	main()
